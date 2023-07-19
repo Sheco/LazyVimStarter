54f88eb03f2812ca5206720532cf6a71e76998e3
Sergio Duran <sergio@tiene.info>
2023-07-19 08:01:57 -0600

    Add <leader>ut keymap to toggle numbers and gutter

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "-", vim.cmd.Ex)
+
+vim.keymap.set("n", "<leader>ut", function()
+  vim.o.relativenumber = not vim.o.relativenumber
+  vim.o.number = not vim.o.number
+  vim.o.signcolumn = vim.o.signcolumn == "yes" and "no" or "yes"
+end, { desc = "Toggle line numbers and gutter" })
