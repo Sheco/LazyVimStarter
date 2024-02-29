07e3ac10b5466cb9f12ff8a6ccef7903a85af109
Sergio Durán <sergio@tiene.info>
2024-02-28 20:36:26 -0600

    clean up core.lua

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,29 +1,9 @@
 return {
-	{
-		"LazyVim/LazyVim",
-		opts = {
-			colorscheme = "catppuccin",
-		}
-	},
-  {
-    "echasnovski/mini.indentscope",
-    enabled = false,
-  },
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    enabled = false,
-  },
-	{
-		"folke/noice.nvim",
-		enabled = false
-	},
-	{
-		"echasnovski/mini.pairs",
-		enabled = false
-	},
-  {
-    "zeek/vim-zeek", enabled = true;
-  },
-  { "folke/flash.nvim", enabled = false },
+	{ "echasnovski/mini.indentscope", enabled = false, },
+	{ "lukas-reineke/indent-blankline.nvim", enabled = false, },
+	{ "folke/noice.nvim", enabled = false },
+	{ "echasnovski/mini.pairs", enabled = false },
+	{ "zeek/vim-zeek", enabled = true; },
+	{ "folke/flash.nvim", enabled = false },
 }
 
