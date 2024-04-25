65bf98f997db0fdb14f96db5baf537a0b79aadb2
Sergio Durán <sergio@tiene.info>
2024-04-25 07:46:41 -0600

    Change - keymap to open neotree instead of netrw

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "-", vim.cmd.Ex)
+vim.keymap.set("n", "-", ':Neotree position=current dir=%:p:h:h reveal_file=%:p<CR>',{noremap = true, silent = true})
 
 vim.keymap.set("n", "<leader>ut", function()
   vim.o.relativenumber = not vim.o.relativenumber
