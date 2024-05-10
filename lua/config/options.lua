8bd2aec6fe6c59c2b5f83d901e4a7d057fb1f4fb
Sergio Durán <sergio@tiene.info>
2024-05-10 07:54:36 -0600

    Enable spell checking

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,8 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.g.spellfile_URL = "https://ftp.nluug.nl/vim/runtime/spell/"
+vim.opt.spelllang = 'en_us'
+-- you can use the following to change to spanish spell checking:
+-- :setlocal spell spelllang=es_mx
