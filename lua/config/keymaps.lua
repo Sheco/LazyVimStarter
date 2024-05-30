51d1816ed28aefccce4659919345721d9769618f
Sergio Durán <sergio@tiene.info>
2024-05-30 15:55:31 -0600

    Remove weird keymapping

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "-", ':Neotree position=current dir=%:p:h:h reveal_file=%:p<CR>',{noremap = true, silent = true})
-
 vim.keymap.set("n", "<leader>ut", function()
   vim.o.relativenumber = not vim.o.relativenumber
   vim.o.number = not vim.o.number
