bac37030e7dae15f93bf5e61d3cb5d424da024f0
Sergio Durán <sergio@tiene.info>
2024-08-08 08:34:31 -0600

    Make tabs' width 2 spaces

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,10 +6,10 @@
 -- vim.opt.mouse = ""
 vim.g.autoformat = false
 
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = false -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.o.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 2 -- Number of spaces inserted when indenting
 
 vim.g.spellfile_URL = "https://ftp.nluug.nl/vim/runtime/spell/"
 vim.opt.spelllang = 'en_us'
